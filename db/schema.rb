4293317a0119d13b8a9da24dd8fc39f44e0c1bc9
k maeda <kmaeda2048@gmail.com>
2019-07-25 11:57:01 +0900

    feat: Create article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_024703) do
+ActiveRecord::Schema.define(version: 2019_07_25_025606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.text "summary"
+    t.datetime "published"
+    t.text "content"
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "feeds", force: :cascade do |t|
     t.string "url"
---
+++ spec/factories/articles.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :article do
+    title { "MyString" }
+    url { "MyString" }
+    summary { "MyText" }
+    published { "2019-07-25 11:56:07" }
+    content { "MyText" }
+    memo { "MyText" }
+  end
+end
