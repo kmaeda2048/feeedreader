f7142f8c21afb59a751414b398c5f5619f670c22
k maeda <kmaeda2048@gmail.com>
2019-07-25 12:00:24 +0900

    feat: Create articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/edit'
   resources :feeds
   root to: 'static_pages#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
