6481be6a923cb889aec2ded580b34c6fce68d743
k maeda <kmaeda2048@gmail.com>
2019-07-25 12:44:56 +0900

    feat: Add #index, #new, #create to feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,14 +1,29 @@
 class FeedsController < ApplicationController
   def index
+    @feeds = Feed.all
+
+    # url = "http://b.hatena.ne.jp/hotentry/it.rss"
+    # xml = HTTParty.get(url).body
+    # @feed = Feedjira.parse(xml)
+
+    # title, url, summary, published, content, サムネイルをどうやって取得するか？
   end
 
   def show
   end
 
   def new
+    @feed = Feed.new
   end
 
   def create
+    @feed = Feed.new(feed_params)
+
+    if @feed.save
+      redirect_to @feed, notice: "「#{@feed.title}」を登録しました。"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +33,11 @@ class FeedsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def feed_params
+    params.require(:feed).permit(:title, :description, :url)
   end
 end
