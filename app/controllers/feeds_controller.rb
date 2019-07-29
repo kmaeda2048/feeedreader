f1450eb388d949a8bedacf5df1b58474991caba0
k maeda <kmaeda2048@gmail.com>
2019-07-29 13:02:09 +0900

    feat: Add #show, #edit, #update, #destroy to feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -10,6 +10,7 @@ class FeedsController < ApplicationController
   end
 
   def show
+    @feed = Feed.find(params[:id])
   end
 
   def new
@@ -27,12 +28,19 @@ class FeedsController < ApplicationController
   end
 
   def edit
+    @feed = Feed.find(params[:id])
   end
 
   def update
+    @feed = Feed.find(params[:id])
+    @feed.update!(feed_params)
+    redirect_to feed_path, notice: "「#{@feed.title}」を更新しました。"
   end
 
   def destroy
+    @feed = Feed.find(params[:id])
+    @feed.destroy
+    redirect_to feeds_path
   end
 
   private
