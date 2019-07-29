e8b9e9d1ddae3329070853e082d68a156a1c7400
k maeda <kmaeda2048@gmail.com>
2019-07-29 16:05:36 +0900

    feat: Add create_articles callback to feed model

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,4 +1,22 @@
 class Feed < ApplicationRecord
+  after_save :create_articles, on: :create
+
   validates :url, presence: true
   validates :title, presence: true
+
+  private
+
+  def create_articles
+    xml = HTTParty.get(self.url).body
+    entry = Feedjira.parse(xml).entries
+
+    entry.each do |e|
+      article = Article.new(title: e.title, url: e.url, published: e.published, content: e.content)
+
+      if article.save
+      else
+      end
+    end
+  end
+
 end
