bdacc8a86545774a444d6ce8f974e780aa1390df
k maeda <kmaeda2048@gmail.com>
2019-07-30 17:02:22 +0900

    feat: Add articles#edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,21 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article, notice: "「#{@article.title}」のメモを更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:memo)
   end
 end
