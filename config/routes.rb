d95db8efac8483340e12b0e3d69dc2b7c587208b
k maeda <kmaeda2048@gmail.com>
2019-07-30 15:52:45 +0900

    feat: Add #index, #show to articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/edit'
+  resources :articles, only: [:index, :show, :edit]
   resources :feeds
   root to: 'static_pages#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
