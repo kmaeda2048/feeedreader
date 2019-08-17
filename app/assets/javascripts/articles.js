8e4d36d6d6d2509762343c652d44b0aeb8878378
k maeda <kmaeda2048@gmail.com>
2019-08-17 09:02:28 +0900

    Fix #67

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -24,17 +24,23 @@ document.addEventListener('turbolinks:load', function () {
                 }
                 break;
             case 'g':
-                targetLink = document.getElementById('focused-card').querySelector('.card-link');
-                window.location.href = targetLink;
-                break;
+                {
+                    const targetLink = document.getElementById('focused-card').querySelector('.card-link');
+                    window.location.href = targetLink;
+                    break;
+                }
             case 't':
-                targetLink = document.getElementById('focused-card').querySelector('.card-link');
-                window.open(targetLink, '_blank');
-                break;
+                {
+                    const targetLink = document.getElementById('focused-card').querySelector('.card-link');
+                    window.open(targetLink, '_blank');
+                    break;
+                }
             case 's':
-                const targetStar = document.getElementById('focused-card').querySelector('.star');
-                targetStar.click();
-                targetStar.classList.toggle('starred');
+                {
+                    const targetStar = document.getElementById('focused-card').querySelector('.star');
+                    targetStar.click();
+                    targetStar.classList.toggle('starred');
+                }
                 break;
             case 'a':
                 window.location.href = document.getElementById('articles-link');
@@ -44,8 +50,6 @@ document.addEventListener('turbolinks:load', function () {
                 break;
         }
     }
-
-    let targetLink;
 
     if (document.querySelector('.card')) {
         const firstCard = document.querySelector('.card');
