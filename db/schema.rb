46549c0d4b948377b1393c2e0d10e9cca0f69fe3
k maeda <kmaeda2048@gmail.com>
2019-09-10 20:51:14 +0900

    Fix #197

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_054941) do
+ActiveRecord::Schema.define(version: 2019_09_10_112545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_054941) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "url", null: false
-    t.string "thumbnail_url", null: false
+    t.string "favicon_url", null: false
     t.index ["feed_url"], name: "index_feeds_on_feed_url", unique: true
   end
 
