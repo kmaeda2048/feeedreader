c76db88eaf33a0c9a936c864bc8dd5b2822c7a06
k maeda <kmaeda2048@gmail.com>
2019-09-12 11:37:12 +0900

    Close #5

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ApplicationRecord
   belongs_to :feed
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[title url]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
