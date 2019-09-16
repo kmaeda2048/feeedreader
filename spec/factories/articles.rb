b846d38d53b7827d2019dd9e71635a0ed94c0f60
k maeda <kmaeda2048@gmail.com>
2019-09-16 23:32:49 +0900

    Add test data

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    url { "MyString" }
-    summary { "MyText" }
-    published { "2019-07-25 11:56:07" }
-    content { "MyText" }
-    memo { "MyText" }
+    title { '記事のタイトル' }
+    url { 'https://github.com/kmaeda2048/feeedreader/commit/45cf4764560dbc1d4a97d3a530d209f2c040731b' }
+    published { '2019-09-14 11:51:32' }
+    unread { true }
+    starred { false }
+    thumbnail_url { 'https://avatars1.githubusercontent.com/u/48312376?s=30&v=4' }
+    feed
   end
 end
--- spec/factories/feeds.rb
+++ spec/factories/feeds.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :feed do
-    url { "MyString" }
-    title { "MyString" }
-    description { "MyText" }
+    feed_url { 'https://github.com/kmaeda2048/feeedreader/commits/master.atom' }
+    name { 'Recent Commits to feeedreader:master	' }
+    url { 'https://github.com/kmaeda2048/feeedreader/commits/master' }
+    favicon_url { 'https://www.google.com/s2/favicons?domain_url=https://github.com/kmaeda2048/feeedreader/commits/master' }
   end
 end
