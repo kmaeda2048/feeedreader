d9b9952b3356b59320e7c8038e58e197298deb0a
k maeda <kmaeda2048@gmail.com>
2019-09-24 11:24:07 +0900

    Fix #275

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,8 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe '.destroy_read_articles' do
+    let!(:feed) { FactoryBot.create(:feed) }
+    let!(:unread_article) { FactoryBot.create(:article, feed: feed) }
+    let!(:read_article) { FactoryBot.create(:article, :read, feed: feed) }
+    let!(:starred_article) { FactoryBot.create(:article, :starred, feed: feed) }
+
+    after do
+      feed.destroy
+    end
+
+    it '既読かつスター付きでない記事のみ削除される' do
+      Article.destroy_read_articles
+      expect(Article.where(id: unread_article.id)).to exist
+      expect(Article.where(id: read_article.id)).not_to exist
+      expect(Article.where(id: starred_article.id)).to exist
+    end
   end
 
   describe '.destroy_overflowing_articles' do
+    let!(:feed) { FactoryBot.create(:feed) }
+    let(:create_size) { 10 }
+    let(:max) { 5 }
+
+    before do
+      create_size.times { FactoryBot.create(:article, feed: feed) }
+    end
+
+    after do
+      feed.destroy
+    end
+
+    it '記事数がmaxになるように削除される' do
+      Article.destroy_overflowing_articles(max)
+      expect(Article.all.size).to eq max
+    end
   end
 end
