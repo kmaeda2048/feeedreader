b69bb248ddce9fb7851230fdc3fb810fca956a63
k maeda <kmaeda2048@gmail.com>
2019-11-12 15:58:56 +0900

    Remove recoverable in user model

---
+++ db/migrate/20191112062336_remove_recoverable_parameter_in_users.rb
@@ -0,0 +1,6 @@
+class RemoveRecoverableParameterInUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :reset_password_token, :string
+    remove_column :users, :reset_password_sent_at, :datetime
+  end
+end
